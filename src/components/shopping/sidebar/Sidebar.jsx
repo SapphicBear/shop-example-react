0a4be78867d68a3da810bc426928eb9bca3338b5
SapphicBear <sleepyscreenart@gmail.com>
2026-03-08 14:48:59 +0100

    Make accordian category area on the sidebar
    All the data is correct based on categories, there are dropdowns
    for showing all products in a certain category filtered by the api's data

--- src/components/shopping/sidebar/Sidebar.jsx
+++ src/components/shopping/sidebar/Sidebar.jsx
@@ -1,23 +1,52 @@
 import styles from "./sidebar.module.css";
+import { useState } from "react";
+
+const Accordian = ({ title, content, id }) => {
+    const [activeId, setActiveId] = useState(null);
+
+    return (
+        <>
+            <div className={styles.accordian}>
+                <div className={styles.accordianCat}>
+                <p 
+                    className={styles.accordianTitle} 
+                    onClick={() => {
+                        activeId === id ? setActiveId(null) : setActiveId(id);
+                    }}>
+                        {title}
+                    </p>
+                    <span className={styles.accordianOpener}>{activeId === id ? "-" : "+"}</span>
+                </div>
+                <ul className={styles.items}>
+                    { activeId === id &&
+                        content.map((item) => {
+                            return (
+                                <li key={item.id} className={styles.product}>{item.title}</li>
+                            );
+                        })}
+                </ul>
+            </div>
+        </>
+    );
+}
 
 const Sidebar = ({ className = `${styles.sidebar}`, products, isLoading, error }) => {
+    const categories = ["men's clothing", "electronics", "jewelery", "women's clothing"];
     return (
         <aside className={className} aria-labelledby="aside-id">
-            <h1 id="aside-id" className={styles.logo}>Look at our wares</h1>
-            <ul className={styles.wares}>
-                {isLoading ? (
-                    <li>Loading...</li>
-                ) : products.length === 0 == false ? (
-                    products.map((item) => {
-                        return (
-                            <li key={item.id} className={styles.product}>
-                                {item.title}
-                            </li>
-                        )
-                    })
-                ) : error ? (
-                    <li>An error occurred...</li>
-                ) : ""}
+            <h1 id="aside-id" className={styles.logo}>Categories</h1>
+            <ul className={styles.accordianArea}>
+            {
+                categories.map((cat, i) => {
+                    return (
+                        <Accordian 
+                            title={cat}
+                            content={products.filter((item) => item.category == cat)}
+                            id={i}
+                        />
+                    )
+                })
+            }
             </ul>
         </aside>
     );
